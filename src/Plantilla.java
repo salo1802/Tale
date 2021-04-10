3a21d572d10a6768d3b9f63225e404b20638555b
Salo1802 <salo1802@hotmail.com>
2021-04-10 18:56:45 -0500

    codigo creado

---
+++ src/Plantilla.java
@@ -0,0 +1,41 @@
+
+
+import processing.core.PApplet;
+
+public class Plantilla extends PApplet{
+	
+	public static void main(String[] args) {
+		PApplet.main("Plantilla");
+	}
+	
+	@Override
+	public void settings() {
+		size(400,400);
+		 
+		 
+	}
+	
+	
+
+	
+	
+	@Override
+	public void setup() {
+	
+		  
+	}
+	
+@Override
+public void draw() {
+	
+	
+	  
+     
+     
+
+     
+    
+	}
+
+ }
+
