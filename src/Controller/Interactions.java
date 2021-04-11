5fa21a5e43f2ac856175f5c16b93c3cce6ee71a0
Salo1802 <salo1802@hotmail.com>
2021-04-11 02:39:39 -0500

    start to load the object into interactions

--- src/Controller/Interactions.java
+++ src/Controller/Interactions.java
@@ -1,5 +1,27 @@
 package Controller;
 
-public class Interactions {
+import Model.Door;
+import Model.Growers;
+import Model.RedHood;
+import processing.core.PApplet;
+import processing.core.PImage;
 
+public class Interactions {
+private boolean allDone;
+ RedHood redhood;
+ Growers teeth,eyes,ears;
+ Door door;
+ PImage lobo;
+ 
+	public Interactions(PApplet app) {
+		allDone = false;
+		redhood = new RedHood(100, 140, 50, 350, app);
+		teeth = new Growers(40, 30, 800, 350, "teeth.png", app);
+		ears = new Growers(130, 40, 800, 350, "ears.png", app);
+		eyes = new Growers(50,10,800,350,"Eyes.png",app);
+		lobo = app.loadImage("LI.png");
+	}
+	public void draw(PApplet app) {
+		
+	}
 }
